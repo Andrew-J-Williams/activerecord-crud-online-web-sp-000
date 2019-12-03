6c36407449acae3a6881bea878d722c0f25f1660
Andrew Williams <1williamsaj@gmail.com>
2019-12-03 03:47:22 +0000

    Automatically backed up by Learn

--- db/migrate/20191203034159_create_movies.rb
+++ db/migrate/20191203034159_create_movies.rb
@@ -5,7 +5,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       x.integer :release_date
       x.string :director
       x.string :boolean
-      x.boolean :
+      x.boolean :in_theaters
     end
   end
 end
