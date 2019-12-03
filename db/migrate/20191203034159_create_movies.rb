6d634617a0e7bcd964c3e59018dcec190414eec8
Andrew Williams <1williamsaj@gmail.com>
2019-12-03 03:46:45 +0000

    Automatically backed up by Learn

--- db/migrate/20191203034159_create_movies.rb
+++ db/migrate/20191203034159_create_movies.rb
@@ -3,7 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |x|
       x.string :title
       x.integer :release_date
-      x.director
+      x.director :
     end
   end
 end
